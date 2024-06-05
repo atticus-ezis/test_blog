d127689e2bf6f01a2363b3e76a9045737d88b585
Atticus Ezis <atticus.ezis@gmail.com>
2024-06-04 23:06:44 -0600

    Added, pub date, comment model, submit form

--- testing_grounds/models.py
+++ testing_grounds/models.py
@@ -1,11 +1,21 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=50, null=True, blank=True)
     text_content = models.TextField()
+    pub_date = models.DateField(default=datetime.now())
 
     def __str__(self):
         return self.title
     
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    text_content = models.TextField()
+    pub_date = models.DateField(default=datetime.now())
+
+    def __str__(self):
+        return self.text_content
+
 
--- testing_grounds/views.py
+++ testing_grounds/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Blog
+from .forms import BlogForm
 
-# Create your views here.
 def index(request):
     blogs = Blog.objects.all()
     context = {'blogs':blogs}
     return render(request, 'testing_grounds/index.html',context)
+
+def form(request):
+    if request.method == 'POST':
+        content = BlogForm(request.POST)
+        if content.is_valid():
+            content.save()
+            return redirect('success')
+    else:
+        content = BlogForm()
+    return render(request, 'submit_form.html', {'content':content})
+
