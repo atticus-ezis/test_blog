b61bf5862daf939f2bc976fe525d0d61b781f272
Atticus Ezis <atticus.ezis@gmail.com>
2024-06-07 17:30:19 -0600

    added registration/login template and login decorator to edit/delete options

--- testing_grounds/urls.py
+++ testing_grounds/urls.py
@@ -4,7 +4,9 @@ from .views import index
 
 urlpatterns = [
     path('', index.as_view(), name="index"),
+    path('register/', views.registerUser, name="register"),
     path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
     path('submit/', views.form, name="form"),
     path('<int:pk>/delete', views.delete, name="delete"),
     path('<int:pk>/edit', views.edit, name='edit'),
--- testing_grounds/views.py
+++ testing_grounds/views.py
@@ -6,14 +6,19 @@ from django.views.generic import ListView, FormView
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import Blog
 from .forms import BlogForm 
 
 def loginPage(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('index')
     if request.method=="POST":
-        username = request.POST.get('username')
+        username = request.POST.get('username')  
         password = request.POST.get('password')
 
         try:
@@ -24,9 +29,30 @@ def loginPage(request):
         if user is not None:
             login(request, user)
             return redirect('index')
+        else:
+            messages.error(request, "Username or password does not exist")
 
-    context = {}
+    context = {'page':page}
     return render(request, 'testing_grounds/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('index')
+
+def registerUser(request):
+    form = UserCreationForm()
+    if request.method=='POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form#.save (commit=False)
+            #user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occured during registration')
+    context = {'form':form}
+    return render(request,'testing_grounds/login_register.html',context)
 
 class index(ListView):
    model = Blog
@@ -45,13 +71,19 @@ def form(request):
         content = BlogForm()
     return render(request, 'testing_grounds/submit_form.html', {'content':content})
 
+@login_required(login_url='login')
 def delete(request, pk):
     blog = Blog.objects.get(id=pk)
+    #if request.user != Blog.host
+        #return HttpResponse('You're not the author')
     blog.delete()
     return HttpResponseRedirect((reverse('index')))
 
+@login_required(login_url='login')
 def edit(request, pk):
     selected_blog = get_object_or_404(Blog, pk=pk)
+    #if request.user != Blog.host
+        #return HttpResponse('You're not the author')
     if request.method == 'POST':
         form = BlogForm(request.POST, instance = selected_blog)
         if form.is_valid():
