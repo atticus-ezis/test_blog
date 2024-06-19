65b8cd23adb76cce8c5f936c5340e4b14369d889
Atticus Ezis <atticus.ezis@gmail.com>
2024-06-18 21:29:46 -0600

    fixed test bug and added test preventing non-authors from deleting

--- testing_grounds/tests.py
+++ testing_grounds/tests.py
@@ -1,15 +1,18 @@
 from django.test import TestCase, Client
 from django.urls import reverse 
 from django.contrib.auth.models import User 
-from models import Blog
+from .models import Blog
 
 class AuthorizedUserDelete(TestCase):
     def setUp(self):
+        # create users
         self.user1 = User.objects.create_user(username='user1', password='AugaHolic101')
         self.user1 = User.objects.create_user(username='user2', password='AugaHolic102')
 
-        self.blog = Blog.objects.create(title='Test Post', content='Test Content', author = self.user1)
+        # create blog
+        self.blog = Blog.objects.create(title='Test Post', text_content = 'Test Content', user = self.user1)
 
+        # create client 
         self.client = Client()
 
     def test_user_can_delete_own_blog_post(self):
@@ -22,6 +25,18 @@ class AuthorizedUserDelete(TestCase):
         # Check that the blog post was deleted
         self.assertEqual(response.status_code, 302)  # Assuming redirect after deletion
         self.assertFalse(Blog.objects.filter(id=self.blog.id).exists())
+
+    def test_user_cant_delete_others_blog_post(self):
+
+        # login as wrong user
+        self.client.login(username='user2', passeord='AguaHolic102')
+
+        response = self.client.post(reverse('delete', args=[self.blog.id, 'blog']))
+
+        self.assertEqual(response.status_code, 302)  # Assuming redirect after deletion
+        self.assertTrue(Blog.objects.filter(id=self.blog.id).exists())
+
+
 
 
 
